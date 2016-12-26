7f777b1e2aec9907aa9c54de2845ed4b04e591dc
Pratyush Singh <singh.pratyush96@gmail.com>
2016-12-26 14:06:21 +0530

    Added season support and fixed url pattern

--- hotstart-playlist-downloader/hotstar-playlist-downloader.py
+++ hotstart-playlist-downloader/hotstar-playlist-downloader.py
@@ -4,11 +4,11 @@ import re
 import requests
 import subprocess
 
-re_link = re.compile(r'.*?hotstar\.com/tv/(?!/).*/[0-9]{4}/episodes/([0-9].*)/'
-                     r'[0-9].*')
+re_link_pl = re.compile(r'.*?hotstar\.com/tv/(?!/).*/[0-9].*/episodes/([0-9].*)/[0-9].*')
+re_link_se = re.compile(r'.*?hotstar.com/tv/(?!/).*?/([0-9].*)/seasons/season-([0-9])')
 
 
-def get_hotstar_links(playlist_id):
+def get_playlist_links(playlist_id):
     url = 'http://search.hotstar.com/AVS/besc?action=SearchContents&appVersi' \
           'on=5.0.37&channel=PCTV&maxResult=10000&moreFilters=series:'\
           + playlist_id \
@@ -18,10 +18,19 @@ def get_hotstar_links(playlist_id):
     request.raise_for_status()
     json_obj = request.json()
     if json_obj['resultCode'] != 'OK':
-        print('Error fetching results. Aborting')
-        sys.exit(1)
+        return []
     return ['http://www.hotstar.com/%s' % x['contentId']
             for x in json_obj['resultObj']['response']['docs']]
+
+
+def get_season_links(season):
+    url = 'http://account.hotstar.com/AVS/besc?action=GetArrayContentList&a' \
+          'ppVersion=5.0.37&categoryId=' + str(season) + '&channel=PCTV'
+    request = requests.get(url)
+    request.raise_for_status()
+    json_obj = request.json()
+    return ['http://www.hotstar.com/%s' % x['contentId']
+            for x in json_obj['resultObj']['contentList']]
 
 
 def download(link):
@@ -37,12 +46,19 @@ def download_many(links):
 
 def main():
     link = sys.argv[1]
-    match = re_link.match(link)
-    if not match:
-        print('Invalid URL')
+    links = []
+    match_se = re_link_se.match(link)
+    if match_se:
+        season, offset = map(int, match_se.groups())
+        final_season = season + offset - 1
+        links += get_season_links(final_season)
+    match_pl = re_link_pl.match(link)
+    if match_pl:
+        playlist_id = match_pl.groups()[0]
+        links += get_playlist_links(playlist_id)
+    if len(links) == 0:
+        print('No links fetched')
         sys.exit(1)
-    playlist_id = match.groups()[0]
-    links = get_hotstar_links(playlist_id)
     download_many(links)
 
 if __name__ == '__main__':
